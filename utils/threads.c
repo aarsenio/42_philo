2493a5b01153380a43c1ea37d99807293e315e85
Alexandre Arsenio ribeiro alves <aarsenio@c1r9s4.42lisboa.com>
2023-01-11 12:01:43 +0000

    fix norm

--- utils/threads.c
+++ utils/threads.c
@@ -6,7 +6,7 @@
 /*   By: aarsenio <aarsenio@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/30 15:33:37 by aarsenio          #+#    #+#             */
-/*   Updated: 2023/01/11 11:04:30 by aarsenio         ###   ########.fr       */
+/*   Updated: 2023/01/11 12:00:43 by aarsenio         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	*routine(void *t)
 
 	philo = t;
 	if (philo->id % 2 == 0)
-		usleep(100);
+		usleep(10000);
 	while (!check_dead() && philo->nbr_times_eat != data()->eat_nbr)
 	{
 		start_eat(philo);
